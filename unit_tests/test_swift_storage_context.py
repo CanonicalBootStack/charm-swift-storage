421336ff461ed606acfe0c5815f291013ada1baa
Drew Freiberger <drew.freiberger@canonical.com>
2020-05-11 15:00:03 -0500

    Add support for object replication handoffs_first
    In extreme cases of object rebalance or node failure, swift environments
    with millions of objects may have a hard time with objects landing in
    handoff or misplaced partitions and the operator of the cloud may need
    to prioritize migrating data in handoff partitions back to primary
    partitions over the 3 primary partitions replicating amongst each other.
    
    To allow for this, the object-server.conf [object-replicator]
    configuration can have handoffs_first set to True which will force
    the object-replicators to focus on handoff partitions before syncing
    primary partitions for any given object partition.
    
    Change-Id: I8b44c287567a0e6d634def0b13baf0fe4ad4aa7b
    Closes-Bug: 1878087

--- unit_tests/test_swift_storage_context.py
+++ unit_tests/test_swift_storage_context.py
@@ -91,6 +91,7 @@ class SwiftStorageContextTests(CharmTestCase):
         self.test_config.set('container-max-connections', '10')
         self.test_config.set('object-max-connections', '10')
         self.test_config.set('object-rsync-timeout', '950')
+        self.test_config.set('object-handoffs-first', True)
         ctxt = swift_context.SwiftStorageServerContext()
         result = ctxt()
         ex = {
@@ -104,6 +105,7 @@ class SwiftStorageContextTests(CharmTestCase):
             'container_max_connections': '10',
             'object_max_connections': '10',
             'object_rsync_timeout': '950',
+            'object_handoffs_first': True,
             'statsd_host': '',
             'statsd_port': 3125,
             'statsd_sample_rate': 1.0
